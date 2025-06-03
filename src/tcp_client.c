2acc03d1927304912964d40b6a7e4ae7ca5f2819
Your Name <you@example.com>
2025-06-03 12:53:06 -0400

    m

--- src/tcp_client.c
+++ src/tcp_client.c
@@ -1,3 +1,38 @@
 #include "tcp_header.h"
 #include <stdio.h>
+#include <string.h>
+
+int tcp_client(const char* hostname, const char* port){
+    struct addrinfo hints;
+    memset(&hints,0,sizeof(hints));
+    hints.ai_family=AF_UNSPEC;
+    hints.ai_socktype=SOCK_STREAM;
+    struct addrinfo* peer_address;
+    int getaddrinfo_res=getaddrinfo(hostname,port,&hints,&peer_address);
+    if(getaddrinfo_res!=0){
+        if(getaddrinfo_res==EAI_SYSTEM){
+            fprintf(stderr,"getaddrinfo() failed. %s, error code %d.\n", strerror(errno),errno);
+        }else{
+            fprintf(stderr,"getaddrinfo() failed. %s, error code %d.\n", gai_strerror(getaddrinfo_res),getaddrinfo_res);
+        }
+        eixt(errno);
+    }
+    
+    int socket_peer=socket(peer_address->ai_family, peer_address->ai_socktype,peer_address->ai_protocol);
+    if(socket_peer<0){
+        fprintf(stderr,"socket() failed. %s, error code %d.\n",strerror(errno),errno);
+        exit(errno);
+    }
+
+    int connect_res=connect(socket_peer,peer_address->ai_addr, peer_address->ai_addrlen);
+    if(connect<0){
+        fprintf(stderr,"connect() failed. %s, error code %d.\n",strerror(errno),errno);
+        exit(errno);
+    }
+    freeaddrinfo(peer_address);
+
+    while(1){
+        
+    }
+}
 
