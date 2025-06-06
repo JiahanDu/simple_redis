6024f62cef7c05ea26c3cd87389571dc5f59db27
Your Name <jiahandu@math.ucla.edu>
2025-06-05 17:36:22 -0700

    m

--- src/tcp_server.c
+++ src/tcp_server.c
@@ -2,6 +2,14 @@
 #include <stdio.h>
 #include <string.h>
 
-int tcp_server(){
+int tcp_server(char* port){
+    struct addrinfo hints;
+    memset(&hints,0,sizeof(hints))
+    hints.ai_family=AF_INET;
+    hints.ai_socktype=SOCK_STREAM;
+    hints.ai_flag=AI_PASSIVE;
+
+    struct addrinfo* bind_address;
+    getaddrinfo(0, port, &hints, &bind_address);
     
 }
