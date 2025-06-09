d355f300b4dab22d4f8cef21bc7c4afdef1c8c1a
Your Name <jiahandu@math.ucla.edu>
2025-06-09 11:26:22 -0400

    m

--- src/tcp_server.c
+++ src/tcp_server.c
@@ -11,5 +11,20 @@ int tcp_server(char* port){
 
     struct addrinfo* bind_address;
     getaddrinfo(0, port, &hints, &bind_address);
+
+    socket socket_listen=socket(bind_address->ai_family, bind_address->ai_socktype, bind_address->ai_protocol);
+    if(socket_listen==-1){
+        fprintf(stderr, "socket() failed. %s, error code %d.\n", strerror(errno),errno);
+    }
+
+    if(bind(socket_listen,bind_address->ai_addr,bind_address->ai_addrlen)){
+        fprintf(stderr, "bind() failed. %s, error code %d.\n", strerror(errno),errno);
+    }
+    freeaddrinfo(bind_address);
+
+    if(listen(socket_listen,10)==-1){
+        fprintf(stderr, "listen() failed. %s, error code %d.\n", strerror(errno),errno);
+    }
+
     
 }
