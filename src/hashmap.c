0985c25de7e9bb027a91af8efd37bfa5dc156eb6
Your Name <jiahandu@math.ucla.edu>
2025-06-09 23:13:27 -0400

    m

--- src/hashmap.c
+++ src/hashmap.c
@@ -1,4 +1,5 @@
 #include "hashmap.h"
+#include <string.h>
 
 unsigned long hash(char* key){
     unsigned long hash=5381;
@@ -12,5 +13,33 @@ unsigned long hash(char* key){
 }
 
 size_t size(HashMap* dict){
-    return dict->_size;
+    return dict->size;
+}
+
+void add(HashMap* dict, char* key, void* val, size_t val_size){
+    unsigned long num=hash(key)%dict->capacity;
+    HashEntry* cur=dict->buckets[num];
+    while(cur->next){
+        cur=cur->next;
+        if(strcmp(cur->key,key)==0){
+            void* cpy=malloc(val_size);
+            cur->val=memcpy()
+            cur->val_size=val_size;
+            break;
+        }
+    }
+    HashEntry* node=malloc(sizeof(HashEntry));
+    if(!node){
+        fprintf(stderr,"malloc() failed. %s, error code %d.\n", strerror(errno),errno);
+        exit(errno);
+    }
+    node->key=strcpy(key);
+    node->val=strcpy(val);
+    node->val_size=val_size;
+    cur->next=node;
+
+    dict->_size+=1;
+    if(dict->_size>RESIZE_FACTOR*dict->capacity){
+        resize(dict);
+    }
 }
--- src/hashmap.h
+++ src/hashmap.h
@@ -5,18 +5,20 @@
 typedef struct HashEntry{
     char* key;
     void* val;
+    size_t val_size;
     struct HashEntry* next;
 } HashEntry;
 
 typedef struct HashMap{
     HashEntry** buckets;
     size_t capacity;
-    size_t _size;
+    size_t size;
 } HashMap;
 
 unsigned long hash(char* key);
+HashMap* initialize(size_t capacity);
 size_t size(HashMap* dict);
-void add(HashMap* dict, char* key, void* val);
+void add(HashMap* dict, char* key, void* val, size_t val_size);
 void remove(HashMap* dict, char* key);
 void* get(HashMap* dict, char* key);
 void resize(HashMap* dict);
