d77d9249783f05fd9268cd7f04c18d88b4abc1dd
Your Name <jiahandu@math.ucla.edu>
2025-06-14 15:45:04 -0700

    m

--- include/skiplist.h
+++ include/skiplist.h
@@ -1,5 +1,24 @@
 #include <stdio.h>
 
-typedef struct{
+typedef struct Node{
+    int val;
+    Node* next;
+    Node* lower;
+    Node* upper;
+}Node;
 
-}skiplist;
+typedef struct Skiplist{
+    Node* bottom;
+    Node* top;
+    int level;
+}Skiplist;
+
+Skiplist* create();
+
+int search(SkipList* obj, int target);
+
+void add(SkipList* obj, int num);
+
+int remove(SkipList* obj, int num);
+
+void skiplistFree(Skilist* obj);
